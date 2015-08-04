e32b21e246e3b75b37f84ba6f5c6fc4dd246e886
MEdwards1989 <matt_31@optusnet.com.au>
2015-08-04 14:45:12 +1000

    Updated ListUnitsCTL.java to meet coding standards - M.E.

--- ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
+++ ITC515_assignment2/src/datamanagement/ListUnitsCTL.java
@@ -1,10 +1,22 @@
 package datamanagement;
+
 public class ListUnitsCTL {
-    private UnitManager um;
-public ListUnitsCTL() {
-        um = UnitManager.UM();
+  
+  private UnitManager unitManager_;
+
+  
+  public ListUnitsCTL() {
+    unitManager_ = UnitManager.UM();
+  }
+
+  
+  public void listUnits(IUnitLister unitLister) {
+    unitLister.clearUnits();
+    
+    UnitMap units = unitManager_.getUnits();
+    for (String s : units.keySet()) {
+      unitLister.addUnit(units.get(s));
+    }
+  }
+  
 }
-            public void listUnits( IUnitLister lister ) {
-lister.clearUnits();UnitMap units = um.getUnits();
-        for (String s : units.keySet() )
-            lister.addUnit(units.get(s));}}
