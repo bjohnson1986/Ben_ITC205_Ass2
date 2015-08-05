02479e5f5808f35b7e6043d40c08ddf9ad715e31
MEdwards1989 <matt_31@optusnet.com.au>
2015-08-05 10:26:38 +1000

    Updated StudentProxy.java to meet coding standards - M.E.

--- ITC515_assignment2/src/datamanagement/StudentProxy.java
+++ ITC515_assignment2/src/datamanagement/StudentProxy.java
@@ -1,63 +1,59 @@
 package datamanagement;
+
 public class StudentProxy implements IStudent {
-private Integer I;
-private String l;
-
-
-
-    private String Il;
-    private StudentManager lI;
-    public StudentProxy( Integer id, String fn, String Il) {
-        this.I = id;
-        this.l = fn;
-
-        
-        this.Il = Il;
-this.lI = StudentManager.get();}
-
-    public Integer getID() { return I; 
-
-    
-    
-}
-public String getFirstName() { 
-        return l; }
-
-    public String getLastName() { 
-return Il; 
-}
-public void setFirstName(String firstName) {
-
-    
-    
-    
-    lI.getStudent(I).setFirstName(firstName);}
-    public void setLastName(String lastName) {
-        
-        
-        
-        lI.getStudent(I).setLastName(lastName);}
-
-    
-    
-        public void addUnitRecord(IStudentUnitRecord record) {
-        lI.getStudent(I).addUnitRecord(record);}
-        public IStudentUnitRecord getUnitRecord(String unitCode) {
-
-            
- 
-            
-            
-            
-            
-            
-                    return lI.getStudent(I).getUnitRecord(unitCode);}
-
-
-        
   
-        
-        
-        
-        
-                                public StudentUnitRecordList getUnitRecords() { return lI.getStudent(I).getUnitRecords();}}
+  private Integer identification_;
+  private String firstName_;
+  private String lastName_;
+  private StudentManager studentManager_;
+  
+
+  public StudentProxy(Integer id, String firstName, String lastName) {
+    this.identification_ = id;
+    this.firstName_ = firstName;
+
+    this.lastName_ = lastName;
+    this.studentManager_ = StudentManager.get();
+  }
+
+  
+  public Integer getID() {
+    return this.identification_;
+  }
+
+  
+  public String getFirstName() {
+    return this.firstName_;
+  }
+
+  
+  public String getLastName() {
+    return this.lastName_;
+  }
+
+  
+  public void setFirstName(String firstName) {
+    this.studentManager_.getStudent(identification_).setFirstName(firstName);
+  }
+
+  
+  public void setLastName(String lastName) {
+    this.studentManager_.getStudent(identification_).setLastName(lastName);
+  }
+
+  
+  public void addUnitRecord(IStudentUnitRecord studentRecord) {
+    this.studentManager_.getStudent(identification_).addUnitRecord(studentRecord);
+  }
+
+  
+  public IStudentUnitRecord getUnitRecord(String unitCode) {
+    return this.studentManager_.getStudent(identification_).getUnitRecord(unitCode);
+  }
+
+  
+  public StudentUnitRecordList getUnitRecords() {
+    return this.studentManager_.getStudent(identification_).getUnitRecords();
+  }
+  
+}
