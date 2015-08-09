4099468b450ed5becf8921cfd920b77ff00f2600
bjohnson1986 <bjohnson@hotmail.com.au>
2015-08-09 21:42:59 +1000

    Fixed bug that was causing unhandled error.

--- ITC515_assignment2/src/datamanagement/StudentUnitRecordManager.java
+++ ITC515_assignment2/src/datamanagement/StudentUnitRecordManager.java
@@ -10,68 +10,69 @@ public class StudentUnitRecordManager {
 	private java.util.HashMap<String, StudentUnitRecordList> subjectToStudentRecord__;
 	private java.util.HashMap<Integer, StudentUnitRecordList> studentToSubjectRecord__;
 
-	//Check if singleton class exists, return self.
+//Check if singleton class exists, return self.
 	public static StudentUnitRecordManager getStudentUnitRecordManager() {
 		if (unitPerStudentRecordManager__ == null)
 			unitPerStudentRecordManager__ = new StudentUnitRecordManager();
 		return unitPerStudentRecordManager__;
 	}
 
-	//Constructor method.
 	private StudentUnitRecordManager() {
 		studentMap__ = new StudentUnitRecordMap();
 		subjectToStudentRecord__ = new java.util.HashMap<>();
 		studentToSubjectRecord__ = new java.util.HashMap<>();
 	}
 
-	//Accessor method to return SutdentUnitRecord. 
+
+	//Accessor method to return StudentUnitRecord. 
 	public IStudentUnitRecord getStudentUnitRecord(Integer studentIdentification, String subjectCode) {
-		IStudentUnitRecord studentPerUnit = studentMap__.get(studentIdentification.toString() + subjectCode);
-		return studentPerUnit != null ? studentPerUnit : setStudentUnitRecord(studentIdentification, subjectCode);
+		IStudentUnitRecord studentPerUnit  = studentMap__.get(studentIdentification.toString() + subjectCode);
+		return studentPerUnit  != null ? studentPerUnit  : setStudentUnitRecord(studentIdentification, subjectCode);
 	}
+
 
 	//Mutator method to change assessment marks.
 	@SuppressWarnings("unchecked")
-	private IStudentUnitRecord setStudentUnitRecord(Integer studentIdentification, String subjectCode) {
+	private IStudentUnitRecord setStudentUnitRecord(Integer subjectCode, String studentIdentification) {
 		IStudentUnitRecord newStudentPerUnit;
 		for (Element element : (List<Element>) XmlManager.getXML().getDocument().getRootElement().getChild("studentUnitRecordTable").getChildren("record")) {
-			
-			//Boolean flag to check if current element has the same studentID and subjectCode in the XML database.
+		//Boolean flag to check if current element has the same studentID and subjectCode in the XML database.
 			boolean isEqualToStudentAndSubject = subjectCode.toString().equals(element.getAttributeValue("sid")) && studentIdentification.equals(element.getAttributeValue("uid"));
 			if (isEqualToStudentAndSubject) {
-				newStudentPerUnit = new StudentUnitRecord(new Integer(element.getAttributeValue("sid")), element.getAttributeValue("uid"), new Float(element.getAttributeValue("asg1")).floatValue(), new Float(element.getAttributeValue("asg2")).floatValue(), new Float(element.getAttributeValue("exam")).floatValue());
+				newStudentPerUnit = new StudentUnitRecord(new Integer(element.getAttributeValue("sid")), element.getAttributeValue("uid"),
+						new Float(element.getAttributeValue("asg1")).floatValue(), new Float(
+								element.getAttributeValue("asg2")).floatValue(), new Float(
+								element.getAttributeValue("exam")).floatValue());
 				studentMap__.put(newStudentPerUnit.getStudentID().toString() + newStudentPerUnit.getUnitCode(), newStudentPerUnit);
 				return newStudentPerUnit;
 			}
 		}
 		throw new RuntimeException(
-				"DBMD: createStudent : student unit record not in file.");
+				"DBMD: createStudent : student unit record not in file");
 	}
 
-	//Get object by unit.
+	
+//Accessor to return a record using subject.
 	@SuppressWarnings("unchecked")
 	public StudentUnitRecordList getRecordsByUnit(String subjectCode) {
-		StudentUnitRecordList studentUnitList = subjectToStudentRecord__.get(subjectCode);
-		//Check if subject exists, and return it if it does.
-		if (studentUnitList != null)
-			return studentUnitList;
-		//Create new record of student against subject.
-		studentUnitList = new StudentUnitRecordList();
+		StudentUnitRecordList unitRecord = subjectToStudentRecord__.get(subjectCode);
+		if (unitRecord != null)
+			return unitRecord;
+		unitRecord = new StudentUnitRecordList();
 		for (Element element : (List<Element>) XmlManager.getXML().getDocument().getRootElement().getChild("studentUnitRecordTable").getChildren("record")) {
-			//Check if current record has the correct subject, and if so add a new record of the subject being undertaken by the student.
 			if (subjectCode.equals(element.getAttributeValue("uid")))
-				studentUnitList.add(new StudentUnitRecordProxy(new Integer(element.getAttributeValue("sid")), element.getAttributeValue("uid")));
+				unitRecord.add(new StudentUnitRecordProxy(new Integer(element.getAttributeValue("sid")), element.getAttributeValue("uid")));
 		}
-		if (studentUnitList.size() > 0)
-			subjectToStudentRecord__.put(subjectCode, studentUnitList); // be careful - this could be empty
-		return studentUnitList;
+		if (unitRecord.size() > 0)
+			subjectToStudentRecord__.put(subjectCode, unitRecord); // be careful - this could be empty
+		return unitRecord;
 	}
 
-	//Get object by student.
+	
+//Accessor to return a record by student.
 	@SuppressWarnings("unchecked")
 	public StudentUnitRecordList getRecordsByStudent(Integer studentIdentification) {
 		StudentUnitRecordList studentRecord = studentToSubjectRecord__.get(studentIdentification);
-		//Check if student exists, and return it if it does.
 		if (studentRecord != null)
 			return studentRecord;
 		//Create new record of student against subject.
